462de6149995e7ebe45c11a11bc2240e0b0d0ab7
davidrobainaartiles <david.robaina.artiles@alumnos.upm.es>
2021-12-07 21:00:43 +0000

    Actualizado MayorLongitudTest y borrado dos ficheros

--- src/test/java/com/practica/cajablanca/MayorLongitudTest.java
+++ src/test/java/com/practica/cajablanca/MayorLongitudTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,12 +14,28 @@ public class MayorLongitudTest {
 
     static Editor editor;
 	
+    @DisplayName("Camino 1")
 	@Test
 	void Test1() throws EmptyCollectionException{
 		editor = new Editor();
 	    assertEquals(null, editor.mayorLongitud());
 	}
 	
+    @DisplayName("Camino 2")
+	@Test
+	@Disabled()
+	void Test2() throws EmptyCollectionException{
+		// si entra en el primer if y también entrará en el primer  for 
+	}
+	
+    @DisplayName("Camino 3")
+	@Test
+	@Disabled()
+	void Test3() throws EmptyCollectionException{
+		// si entra en el primer for tambien entrará en el segundo
+	}
+	
+    @DisplayName("Camino 4")
 	@Test
 	void Test4() throws EmptyCollectionException{
 		editor = new Editor();
@@ -26,17 +43,26 @@ public class MayorLongitudTest {
 	    assertEquals("HOLA", editor.mayorLongitud());
 	}
 	
+    @DisplayName("Camino 5")
 	@Test
+	@Disabled()
 	void Test5() throws EmptyCollectionException{
-		editor = new Editor();
-		editor.leerFichero("ficheroCamino5.txt");
-	    assertEquals("ADIOS", editor.mayorLongitud());
+		// siempre se ejecutará el primer if si se entra en el segundo for
 	}
 	
+    @DisplayName("Camino 6")
 	@Test
+	@Disabled()
 	void Test6() throws EmptyCollectionException{
-		editor = new Editor();
-		editor.leerFichero("ficheroCamino6.txt");
-	    assertEquals("ADIOS", editor.mayorLongitud());
+		// siempre se ejecutará el primer if si se entra en el segundo for
 	}
+    
+	@DisplayName("Prueba adicional")
+	@Test
+	void Test7() throws EmptyCollectionException{
+		editor = new Editor();
+		editor.leerFichero("fichero.txt");
+	    assertEquals("FERNANDEZ", editor.mayorLongitud());
+	}
+	
 }

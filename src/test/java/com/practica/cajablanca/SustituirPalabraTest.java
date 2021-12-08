8ce8aaaf4e622e86fe6b0b81b93e163d02f5d1ae
davidrobainaartiles <david.robaina.artiles@alumnos.upm.es>
2021-12-08 12:19:03 +0000

    Agregado test de SustituirPalabra()

---
+++ src/test/java/com/practica/cajablanca/SustituirPalabraTest.java
@@ -0,0 +1,55 @@
+package com.practica.cajablanca;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.cajanegra.EmptyCollectionException;
+
+public class SustituirPalabraTest {
+    
+	static Editor editor;
+	
+    @DisplayName("Camino 1")
+	@Test
+	void Test1() throws EmptyCollectionException{
+		editor = new Editor();
+		Editor editorAux = new Editor();
+	    editor.sustituirPalabra("HOLA","ADIOS");
+	    assert(editor.editIsEmpty());
+	}
+    
+    @DisplayName("Camino 2")
+	@Test
+	@Disabled()
+	void Test2() throws EmptyCollectionException{
+		// si el editor no está vacío, la condición del while no puede ser false
+	}
+   
+    @DisplayName("Camino 3")
+	@Test
+	@Disabled()
+	void Test3() throws EmptyCollectionException{
+		// si el editor no está vacío, la condición del while no puede ser false
+	}
+    
+    @DisplayName("Camino 4")
+	@Test
+	void Test4() throws EmptyCollectionException{
+		editor = new Editor();
+		editor.leerFichero("ficheroConUnaPalabra.txt");
+	    editor.sustituirPalabra("HOLA","ADIOS");
+	    assertEquals("[ADIOS]", editor.getLinea(1).toString());
+	}
+    
+    @DisplayName("Camino 5")
+	@Test
+	void Test5() throws EmptyCollectionException{
+		editor = new Editor();
+		editor.leerFichero("ficheroConUnaPalabra.txt");
+	    editor.sustituirPalabra("ADIOS","HOLA");
+	    assertEquals("[HOLA]", editor.getLinea(1).toString());
+	}
+}
